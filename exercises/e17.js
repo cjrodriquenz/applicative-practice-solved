da1fa0fab1bec04fc29c9c88901d906cd27bfebe
Cameron Rodriquenz <cjrodriquenz@gmail.com>
2025-03-08 19:23:42 -0500

    solved exercises 17 and 18

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,7 +1,7 @@
 /**
  * Make sure to read the e17.md file!
- * For this exercise we want you to make custom `maxBy` and `minBy` methods. 
- * In order to do this it is ok to use any type of FOR loops. 
+ * For this exercise we want you to make custom `maxBy` and `minBy` methods.
+ * In order to do this it is ok to use any type of FOR loops.
  * REQUIREMENTS: functions should:
  * * Take an array of things
  * * minBy(): Return the element where the callback function on that element yields the lowest value
@@ -10,14 +10,37 @@
 
 export function minBy(array, cb) {
   // Your code goes here...
+  if (array.length === 0) return undefined;
 
+  let minElement = array[0];
+  let minValue = cb(array[0]);
+
+  for (let i = 1; i < array.length; i++) {
+    const value = cb(array[i]);
+    if (value < minValue) {
+      minValue = value;
+      minElement = array[i];
+    }
+  }
+  return minElement;
 }
 
 export function maxBy(array, cb) {
   // Your code goes here...
+  if (array.length === 0) return undefined;
 
+  let maxElement = array[0];
+  let maxValue = cb(array[0]);
+
+  for (let i = 1; i < array.length; i++) {
+    const value = cb(array[i]);
+    if (value > maxValue) {
+      maxValue = value;
+      maxElement = array[i];
+    }
+  }
+  return maxElement;
 }
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-17"
--- exercises/e18.js
+++ exercises/e18.js
@@ -3,10 +3,29 @@
  * Return the year with the greatest number of Asteroids discoveries
  * Return example: 1902
  */
-
+import { maxBy } from "./e17";
 export function getGreatestDiscoveryYear(data) {
   // Your code goes here...
   // feel free to import your `maxBy` or `minBy` methods from previous lessons
+  const discoveryCounts = {};
+
+  data.asteroids.forEach((asteroid) => {
+    const year = asteroid.discoveryYear;
+    if (discoveryCounts[year]) {
+      discoveryCounts[year]++;
+    } else {
+      discoveryCounts[year] = 1;
+    }
+  });
+
+  const discoveryArray = Object.keys(discoveryCounts).map((year) => ({
+    year: parseInt(year),
+    count: discoveryCounts[year],
+  }));
+
+  const greatestDiscoveryYear = maxBy(discoveryArray, (item) => item.count);
+
+  return greatestDiscoveryYear.year;
 }
 
 // === TEST YOURSELF ===
